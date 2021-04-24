634658e476f7149f9e64446d5905e04244a909f7
Hailyn Mora <hailinmora.1897@gmail.com>
2021-04-23 21:12:55 -0500

    croquis

---
+++ resources/views/barra.blade.php
@@ -0,0 +1,35 @@
+
+    <nav class="navbar navbar-expand-lg  navbar-success bg-success">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Nosotros</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0" >
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Estadisticas</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Certificados</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Censo
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Registrar</a></li>
+                        <li><a class="dropdown-item" href="#">Actualizar</a></li>
+                        <li><a class="dropdown-item" href="#">Listar</a></li>
+                        <li><a class="dropdown-item" href="#">Buscar</a></li>
+                    </ul>
+                </li>
+                
+            </ul>
+            <form class="d-flex">
+                <button class="btn btn-outline-warning" type="submit">Iniciar Sesion</button>
+            </form>
+        </div>
+    </div>
+    </nav>
+ 
---
+++ resources/views/pie.blade.php
@@ -0,0 +1,48 @@
+<footer class="bg-success text-center text-lg-start">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!--Grid row-->
+    <div class="row">
+      <!--Grid column-->
+      <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase"><b>Contactos</b></h5>
+
+        <p>
+        315 6008858
+        </p>
+        <p>
+        cabildolamontana@gmail.com
+        </p>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase"><b>Dirección</b></h5>
+
+        <p>
+        Barrio Nuevo Horizonte, Calle 5, Cl. 5 #626a
+        Samaniego, Nariño
+        Colombia
+        </p>
+      </div>
+      <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase"><b>Horario de Atención</b></h5>
+
+        <p>
+          Lunes a Viernes 
+            08:00 am - 06:00 pm
+        </p>
+      </div>
+      <!--Grid column-->
+    </div>
+    <!--Grid row-->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2021
+  </div>
+  <!-- Copyright -->
+</footer>
